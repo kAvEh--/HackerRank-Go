c2b4147a0596301b0ca8f7b252ebe12203b591e0
kaveh <kaveh.fazaeli@gmail.com>
2021-01-06 13:12:53 +0330

    Queues: A Tale of Two Stacks

---
+++ interview/queues.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create("input.txt")
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	tTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	c := make([]string, int(t))
+	start, end := 0, 0
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		s := readLine(reader)
+
+		tmp := strings.Fields(s)
+		if tmp[0] == "1" {
+			c[end] = tmp[1]
+			end++
+		} else if tmp[0] == "2" {
+			start++
+		} else {
+			fmt.Fprintf(writer, "%s\n", c[start])
+		}
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
