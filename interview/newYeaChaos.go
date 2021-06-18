248ed611bfca287493d5e9f06e6c9719e9438d83
kaveh <kaveh.fazaeli@zoodfood.com>
2021-06-18 18:15:13 +0430

    New Year Chaos

---
+++ interview/newYeaChaos.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'minimumBribes' function below.
+ *
+ * The function accepts INTEGER_ARRAY q as parameter.
+ */
+
+func minimumBribes(q []int32) {
+	result := int32(0)
+	for i := int32(len(q)) - 1; i >= 0; i-- {
+		if q[i]-(i+1) > 2 {
+			fmt.Println("Too chaotic")
+			return
+		}
+		for j := max2(0, q[i]-2); j < i; j++ {
+			if q[j] > q[i] {
+				result++
+			}
+		}
+	}
+	fmt.Println(result)
+}
+
+func max2(a, b int32) int32 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	tTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+		checkError(err)
+		n := int32(nTemp)
+
+		qTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+		var q []int32
+
+		for i := 0; i < int(n); i++ {
+			qItemTemp, err := strconv.ParseInt(qTemp[i], 10, 64)
+			checkError(err)
+			qItem := int32(qItemTemp)
+			q = append(q, qItem)
+		}
+
+		minimumBribes(q)
+	}
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
